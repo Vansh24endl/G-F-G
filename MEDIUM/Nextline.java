f0bdd9c35bd103d62318f336bea0b2c8b06fd05d
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-03-11 21:08:06 +0530

    Create Nextline.java

---
+++ MEDIUM/Nextline.java
@@ -0,0 +1,17 @@
+class Geeks {
+    static void printIndividualLine() {
+        System.out.println("Geeks" + "\nfor" + "\nGeeks");
+    }
+}
+
+/*Driver Code Starts. 
+class GfG {
+public static void main(String args[]) 
+{
+// Creating an object of Class Geeks
+  Geeks g
+  new Geeks();
+// Calling a function of Class Geeks
+  g. printlndividua1Line();
+  System. out. println ( "v' ) ;
+} Driver Code Endsl*/
