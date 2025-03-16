a0f74cd848d3c5dbbf9073ffcbeb32521c0d7921
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-03-16 21:07:41 +0530

    Create MissingInArray.java

---
+++ EASY/MissingInArray/MissingInArray.java
@@ -0,0 +1,21 @@
+// Driver Code
+
+class solution
+{
+    int MissingNumber(int arr[])
+    {
+      int n = arr.length + 1;
+      int xorResult = 0;
+
+      for(int i = 1; i <= n; i++)
+        {
+          xorResult ^= i;
+        }
+      for(int n : arr)
+        {
+          xorResult ^= num;
+        }
+
+      result xorResult;
+    }
+}
